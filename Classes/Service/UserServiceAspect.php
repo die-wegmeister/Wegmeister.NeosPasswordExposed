53b06ce8d4503c64d2aa00f90de88dbaf6af79a1
Benjamin Klix <benjamin.klix@die-wegmeister.com>
2024-02-22 16:28:28 +0100

    UPDATE: Add join points for addUser and setUserPassword

--- Classes/Service/UserServiceAspect.php
+++ Classes/Service/UserServiceAspect.php
@@ -10,6 +10,33 @@ use Neos\Flow\Validation\Exception as ValidationException;
 #[Flow\Aspect]
 class UserServiceAspect
 {
+    /** @var VariableFrontend */
     #[Flow\Inject]
-    protected VariableFrontend $cacheFrontend;
+    protected $cache;
+
+    /**
+     * @param JoinPointInterface $joinPoint
+     * @return string
+     * @throws ValidationException
+     */
+    #[Flow\Around("method(Neos\Neos\Domain\Service\UserService->addUser()) && setting(Wegmeister.NeosPasswordExposed.checkExposedPasswords)")]
+    public function addUserWithExposedPasswordCheck(JoinPointInterface $joinPoint)
+    {
+        throw new ValidationException('This is working.');
+
+        return $joinPoint->getAdviceChain()->proceed($joinPoint);
+    }
+
+    /**
+     * @param JoinPointInterface $joinPoint
+     * @return string
+     * @throws ValidationException
+     */
+    #[Flow\Around("method(Neos\Neos\Domain\Service\UserService->setUserPassword()) && setting(Wegmeister.NeosPasswordExposed.checkExposedPasswords)")]
+    public function setUserPasswordWithExposedPasswordCheck(JoinPointInterface $joinPoint)
+    {
+        throw new ValidationException('This is working.');
+
+        return $joinPoint->getAdviceChain()->proceed($joinPoint);
+    }
 }
